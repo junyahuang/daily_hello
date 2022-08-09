727a63f8149208065b52d5642851770e9b5987d7
junyahuang <smaite96@outlook.com>
2022-08-09 23:01:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
 keyday = os.environ['KEYDAY']
-
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
